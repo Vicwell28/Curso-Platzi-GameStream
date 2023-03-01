67c5cb1c107078354c789f64e40fcbe35777363a
Vicwell28 <vicwell.bj@gmail.com>
2023-03-01 15:36:50 -0600

    init

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by soliduSystem on 01/03/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
